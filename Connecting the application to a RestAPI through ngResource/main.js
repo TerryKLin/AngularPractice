640cf9e797cf77eb2446b162623116e8a89ff012
Terry <terrylin1990k@gmail.com>
2016-05-04 11:26:07 -0300

    Implemented client side sorting
    - Used angular’s $watch to monitor the search changes
    - Implemented 2 services to do the search and order magics

--- Connecting the application to a RestAPI through ngResource/main.js
+++ Connecting the application to a RestAPI through ngResource/main.js
@@ -31,6 +31,19 @@ app.controller('PersonListController', function ($scope, ContactService) {
 		console.log("Load More");
 		$scope.contacts.loadMore();
 	};
+	$scope.$watch('search',function(newVal,oldVal){
+		//Check if new value is defined, since it msut be defined
+		if(angular.isDefined(newVal)){
+			$scope.contacts.doSearch(newVal);
+		}
+	})
+
+	$scope.$watch('order',function(newVal,oldVal){
+		//Check if new value is defined, since it msut be defined
+		if(angular.isDefined(newVal)){
+			$scope.contacts.doOrder(newVal);
+		}
+	})
 });
 
 app.service('ContactService', function (Contact) {
@@ -46,6 +59,23 @@ app.service('ContactService', function (Contact) {
 		'isLoading':false, // These 3 are for paginating data
 		'selectedPerson': null,
 		'persons': [],
+		'search': null,
+		//Reset the first three variables
+		'doSearch': function(search){
+			self.hasMore = true;
+			self.page = 1;
+			self.persons = [];
+			self.search = search;
+			self.loadContacts();
+		},
+		'doOrder': function(order){
+			self.hasMore = true;
+			self.page = 1;
+			self.persons = [];
+			//API is expecting the name ordering
+			self.ordering = order;
+			self.loadContacts();
+		},
 		'loadContacts': function(){
 			//Wrap the function inside the if statement and run it if there's more data and isLoading function is not called
 			if(self.hasMore && !self.isLoading){
@@ -53,7 +83,9 @@ app.service('ContactService', function (Contact) {
 
 				// Include this in the indicate the page number we want to 
 				var params = {
-					'page':self.page
+					'page':self.page,
+					'search':self.search,
+					'ordering':self.ordering
 				};
 				// The first parameter will be sent as query parameter at the end of the URL
 				Contact.get(params,function(data){
