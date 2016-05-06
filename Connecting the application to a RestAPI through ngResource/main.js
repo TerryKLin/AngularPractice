409664a5564248b4ce57be16e382250e645460db
Terry <terrylin1990k@gmail.com>
2016-05-06 14:51:16 -0300

    Replaced the number column with images and the birthdate column with phone number, added a notification message after one creates or deletes a person, and a better looking date picker

--- Connecting the application to a RestAPI through ngResource/main.js
+++ Connecting the application to a RestAPI through ngResource/main.js
@@ -4,8 +4,9 @@ var app = angular.module('codecraft', [
 	'angularSpinner',
 	'jcs-autoValidate',
 	'angular-ladda',
-	'mgcrea.ngStrap'
-
+	'mgcrea.ngStrap',
+	'toaster',
+	'ngAnimate'
 ]);
 
 //Config is called before the $http service 
@@ -84,7 +85,7 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
 });
 
 // Inject the q service, which enables you to create promise that can be returned as function
-app.service('ContactService', function (Contact,$q) {
+app.service('ContactService', function (Contact,$q,toaster) {
 	var self = {
 		'addPerson': function (person) {
 			this.persons.push(person);
@@ -154,6 +155,7 @@ app.service('ContactService', function (Contact,$q) {
 			self.isSaving = true;
 			person.$update().then(function(){
 				self.isSaving = false;
+				toaster.pop('success','Updated' + person.name);
 			});
 		},
 		'removeContact':function(person){
@@ -164,6 +166,7 @@ app.service('ContactService', function (Contact,$q) {
 				var index = self.persons.indexOf(person);
 				self.persons.splice(index,1);
 				self.selectedPerson = null;
+				toaster.pop('success','Deleted' + person.name);
 			});
 		},
 		'createContact': function(person){
@@ -176,6 +179,7 @@ app.service('ContactService', function (Contact,$q) {
 				self.page = 1;
 				self.persons = [];
 				self.loadContacts()
+				toaster.pop('success','Created' + person.name);
 				d.resolve()
 			});
 			return d.promise;
